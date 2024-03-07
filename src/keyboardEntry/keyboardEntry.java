ba40d01761e14a4c5093c915746565e38d838401
Dondamart <daniyermo@gmail.com>
2024-03-07 12:25:43 +0100

    KeyboardEntry

---
+++ src/keyboardEntry/keyboardEntry.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package keyboardEntry;
+
+/**
+ *
+ * @author danim
+ */
+public class keyboardEntry {
+
+    public static void main(String[] args) {
+        System.out.println("Starting program...");
+        System.out.println("Enter your name: ");
+
+    }
+    
+}
